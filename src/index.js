42b7014fa3923cec46c8a668b4e0aa3fe284d1c3
yamasaki yu <my10290430@icloud.com>
2022-08-03 12:43:16 +0000

    Update 12 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import "./styles.css";
+var val1 = "var変数";
+console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+val1 = "上書き";
+console.log(val1);
+
+var val1 =  "varウェイ";
+cosole.log(val1);
